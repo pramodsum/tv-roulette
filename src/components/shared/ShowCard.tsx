8958786875924b26186e8135bb830d61a2d86752
Sumedha Pramod <sumedha.pramod@gmail.com>
2020-03-26 16:47:20 -0400

    Fix internal links

--- src/components/shared/ShowCard.tsx
+++ src/components/shared/ShowCard.tsx
@@ -10,28 +10,34 @@ import {
 } from "@material-ui/core";
 
 import { Series } from "../../declarations/types";
+import { Link } from "react-router-dom";
 
 const ShowCard: React.FC<Series> = props => (
   <Box width="30%" my={1}>
     <Card>
-      <CardActionArea href={`/tv-roulette/${props.id}`}>
-        <CardMedia
-          style={{ height: "140px" }}
-          image={`https://image.tmdb.org/t/p/w500_and_h282_face${props.poster_path}`}
-          title={props.name}
-        />
-        <CardContent>
-          <Box display="flex" justifyContent="space-between">
-            <Typography gutterBottom variant="h5" component="h2">
-              {props.name}
-            </Typography>
-            <Chip
-              color="primary"
-              style={{ marginTop: "2px", marginLeft: "6px" }}
-              label={`${props.vote_average * 10}%`}
-            />
-          </Box>
-        </CardContent>
+      <CardActionArea>
+        <Link
+          to={`/${props.id}`}
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <CardMedia
+            style={{ height: "140px" }}
+            image={`https://image.tmdb.org/t/p/w500_and_h282_face${props.poster_path}`}
+            title={props.name}
+          />
+          <CardContent>
+            <Box display="flex" justifyContent="space-between">
+              <Typography gutterBottom variant="h5" component="h2">
+                {props.name}
+              </Typography>
+              <Chip
+                color="primary"
+                style={{ marginTop: "2px", marginLeft: "6px" }}
+                label={`${props.vote_average * 10}%`}
+              />
+            </Box>
+          </CardContent>
+        </Link>
       </CardActionArea>
     </Card>
   </Box>
