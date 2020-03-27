9f9b8c62e29e33cecfd2eaee33f261bb3ee892b0
Sumedha Pramod <sumedha.pramod@gmail.com>
2020-03-27 00:08:59 -0400

    Add google analytics

--- src/components/shared/layout/Layout.tsx
+++ src/components/shared/layout/Layout.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Toolbar, Fab } from "@material-ui/core";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import ReactGA from "react-ga";
 
 import Header from "../header/Header";
 import ScrollTop from "./ScrollTop";
+import { useLocation } from "react-router-dom";
 
 const Layout: React.FC = ({ children, ...props }) => {
+  const location = useLocation();
+  ReactGA.initialize("UA-162050139-1");
+  ReactGA.pageview(location.pathname + location.search);
+
   return (
     <>
       <Header />
