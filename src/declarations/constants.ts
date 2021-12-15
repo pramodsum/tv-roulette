639acca7404d9fef6acd520c303237119130dfdb
Sumedha Pramod <sumedha.pramod@gmail.com>
2021-12-15 12:16:49 -0800

    Move to nextjs app

--- src/declarations/constants.ts
+++
@@ -1,5 +0,0 @@
-export const MOVIEDB_API_KEY = "5c79f5aa81b00c7995fd908b3b13913e";
-export const MOVIEDB_API_URL_BASE = "https://api.themoviedb.org/3";
-export const TRAKT_API_KEY =
-  "348b4373bf3edcf72fc2b5684f7488298449bb177ccb7c82399e3122116282eb";
-export const TRAKT_API_URL_BASE = "https://api.trakt.tv";
--- src/declarations/types.ts
+++
@@ -1,6 +0,0 @@
-export type FilterObj = {
-  filter: "watched" | "popular" | "trending";
-  title: string;
-};
-
-export type TimeFrame = "Weekly" | "Monthly" | "Yearly" | "All";
